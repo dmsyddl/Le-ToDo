9bfc5895eaf35daa448a62d5e93ae1d35319591d
eunyoung <shineeol20@gmail.com>
2022-08-22 16:42:06 +0900

    feat: 숫자 업데이트 기능

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseHandler = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseHandler = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="count--container">
+          {count}
+          <div className="count--button">
+            <button onClick={increaseHandler}>+1</button>
+            <button onClick={decreaseHandler}>-1</button>
+          </div>
+        </div>
       </header>
     </div>
   );
